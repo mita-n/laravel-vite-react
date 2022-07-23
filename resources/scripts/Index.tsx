4666dad43a0ea3b7a75dc9eda32969d99a7cec46
mita-n <98956240+mita-n@users.noreply.github.com>
2022-07-23 20:18:27 +0900

    nihehe_shippai

--- resources/scripts/Index.tsx
+++ resources/scripts/Index.tsx
@@ -3,25 +3,39 @@ import * as ReactDom from 'react-dom';
 
 const Index = () => {
 
+    //16時だけなぜかユリ熊になる　ここで現在時間を取得
     let now = new Date().getHours();
-
+    
+    //数字をランダムにカウントする
+    //const [状態変数, 状態を変更するための関数] = useState(状態の初期値);
     const [count, setCount] = useState(0);
-
     const handleClick = () => {
         // Hooksを用いているのでthisを使わなくてもstateを参照できる
-        setCount(() => count + 1);
+        setCount(() => count + Math.floor(Math.random() * 100));
     };
+    //数字をランダムにカウントする ここまで
+
+    const [nihehe, nihe] = useState('舐花');
+    const arusutoromeria = ['甘奈','千雪','舐花'];
+    const randomSummer = () => {
+        arusutoromeria.map((nihe, index) =>{
+            return nihe;
+        })
+    }
+    console.log(nihe)
 
     if (now !== 16) {
         return (
-            <div>
+            <>
                 <p>
                     私ポンコツアンドロイドぉ
                     たすけてぇ
                 </p>
-                <p>You clicked {count} times</p>
-                <button onClick={handleClick}>+1</button>
-            </div>
+                <p>クリックしたらランダムに増える</p>
+                <li> {count}</li>
+                <button onClick={handleClick}>random</button>
+                <button onClick={randomSummer}>{nihehe}</button>
+            </>
         );
     }else {
         return (
