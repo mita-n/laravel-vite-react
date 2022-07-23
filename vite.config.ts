1ca5387948a8d524a2a137d421e9d0886766f84b
mita-n <98956240+mita-n@users.noreply.github.com>
2022-07-23 15:37:41 +0900

    modify-vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import laravel from 'vite-plugin-laravel'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
 	plugins: [
-		laravel({
-			postcss: [
-				tailwindcss(),
-				autoprefixer(),
-			],
-		}),
+		laravel(),
+		reactRefresh(),
 	],
 })
